bba1c4f45daeedf4da62b1f2392ea93e7392d06d
nosami <jasonimison@gmail.com>
2018-04-12 11:19:41 +0100

    Update to use FormsAppCompatActivity

--- Droid/MainActivity.fs
+++ Droid/MainActivity.fs
@@ -8,11 +8,14 @@ open Android.Runtime
 open Android.Views
 open Android.Widget
 open Android.OS
+open Xamarin.Forms.Platform.Android
 
-[<Activity (Label = "TicTacToe.Droid", Icon = "@drawable/icon", MainLauncher = true, ConfigurationChanges = (ConfigChanges.ScreenSize ||| ConfigChanges.Orientation))>]
+[<Activity (Label = "TicTacToe.Droid", Icon = "@drawable/icon", Theme = "@style/MyTheme", MainLauncher = true, ConfigurationChanges = (ConfigChanges.ScreenSize ||| ConfigChanges.Orientation))>]
 type MainActivity() =
-    inherit Xamarin.Forms.Platform.Android.FormsApplicationActivity()
+    inherit FormsAppCompatActivity()
     override this.OnCreate (bundle: Bundle) =
+        FormsAppCompatActivity.TabLayoutResource <- Resources.Layout.Tabbar
+        FormsAppCompatActivity.ToolbarResource <- Resources.Layout.Toolbar
         base.OnCreate (bundle)
 
         Xamarin.Forms.Forms.Init (this, bundle)
